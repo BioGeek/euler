f08ef831056ad08aa8845de78a4fd81843e96f2d
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2011-08-23 05:53:49 +0200

    added problem 92

--- problem046.py
+++
@@ -1,5 +0,0 @@
-def isOddComposite(n):
-    """An odd number is an integer that is not evenly divisible by 2. 
-    A composite number is a positive number that has divisors other than 1 and itself. 
-    So any odd number greater than 1 that is not prime would be a composite odd number. """
-    
